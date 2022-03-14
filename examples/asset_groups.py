75c8563626074bd978857ae48b7b9ebc9596e7a2
Austin de Coup-Crank <Austin.deCoupCrank@moodys.com>
2022-03-14 08:45:08 -0500

    sort arg list

--- examples/asset_groups.py
+++ examples/asset_groups.py
@@ -7,6 +7,6 @@ qualys = pyqualys.Qualys(
 )
 
 group_id = '12249485'
-response = qualys.update_asset_group(group_id=group_id, add_ips="172.16.16.254")
+response = qualys.update_asset_group(group_id=group_id, set_ips="172.16.16.254,10.2.3.4,192.168.1.1")
 pprint(response)
-import ipdb; ipdb.set_trace()
+#import ipdb; ipdb.set_trace()
